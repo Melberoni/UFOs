96333e5d334669324e5bf1dba8168c024211aa11
Robertson <Robertson.Melissa1@mayo.edu>
2021-12-06 19:23:48 -0600

    completed module 11 before challenge

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,19 @@
 // import the data from data.js
 const tableData=data;
 // reference the html table using d3
-var tbody =d3.select("tbody");
+var tbody = d3.select("tbody");
 
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
     // Next, loop through each object in the data
   // and append a row and cells for each value in the row
-    data.forEach((dataRow)=>{
+    data.forEach((dataRow) => {
         // Append a row to the table body
-        let row = body.append("tr");
+        let row = tbody.append("tr");
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
-        Object.values(dataRow).forEach((val)=>{
+        Object.values(dataRow).forEach((val) => {
             let cell=row.append("td");
             cell.text(val);
         }
@@ -29,8 +29,8 @@ function handleClick(){
     if (date){
         // Apply `filter` to the table data to only keep the
         // rows where the `datetime` value matches the filter value
-        filteredData=filteredData.filter(row=> row.datetime === date);
-    };
+        filteredData=filteredData.filter(row => row.datetime === date);
+    }
     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
